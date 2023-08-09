2772efd56a24907c8de8c6459bf016c55bb08a23
Tristan Gerrish <tristan.gerrish@burohappold.com>
2023-08-09 16:22:31 +0100

    Added pre-build to move over prototypes

---
+++ LadyBugTools_Prototype_Engine/Python/example/method.py
@@ -0,0 +1,14 @@
+
+def example_method(a: float, b: float) -> float:
+    """Example method
+    
+    Args:
+        a (float):
+            The first number.
+        b (float):
+            The second number.
+    Return:
+        float: The sum of the two numbers.
+    """
+    
+    return a + b
