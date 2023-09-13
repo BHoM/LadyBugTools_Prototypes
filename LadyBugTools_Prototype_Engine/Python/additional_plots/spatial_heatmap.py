e3591ff324aec726a3f7f08bfb91724af933e687
Tristan Gerrish <tristan.gerrish@burohappold.com>
2023-09-13 21:19:42 +0100

    Added new triangulation and grid handling methods

--- LadyBugTools_Prototype_Engine/Python/additional_plots/spatial_heatmap.py
+++ LadyBugTools_Prototype_Engine/Python/additional_plots/spatial_heatmap.py
@@ -27,6 +27,161 @@ from matplotlib.figure import Figure
 from matplotlib.tri.triangulation import Triangulation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+from honeybee_radiance.sensorgrid import SensorGrid, Sensor
+from honeybee_radiance.view import View
+from ladybugtools_toolkit.plot.utilities import create_triangulation
+from honeybee_radiance_folder.folder import ModelFolder
+import json
+from ladybug_geometry.geometry3d.plane import Plane, Point3D
+from scipy.spatial.distance import cdist
+from warnings import warn
+
+
+def sensorgrids_from_results_folder(results_folder: Path) -> Tuple[SensorGrid]:
+    """Create a list of sensorgrids from a results folder.
+
+    Args:
+        results_folder (Path): A directory contianing radiance simulation results from a lbt-recipe.
+
+    Returns:
+        Tuple[SensorGrid]: A tuple of honeybee-radiance SensorGrids.
+    """
+    if not isinstance(results_folder, Path):
+        results_folder = Path(results_folder)
+
+    # check that one of the following directories exists
+    res_dirs = [
+        "annual_daylight",
+        "annual_irradiance",
+        "cumulative_radiation",
+        "daylight_factor",
+        "direct_sun_hours",
+        "sky_view",
+        "point_in_time_grid",
+    ]
+    for res_dir in res_dirs:
+        if (results_folder / res_dir).exists():
+            # load pts if they exist
+            sensor_grids = []
+            for i in ModelFolder(project_folder=results_folder / res_dir).grid_files():
+                sensor_grids.append(SensorGrid.from_file(results_folder / res_dir / i))
+            return sensor_grids
+
+    raise ValueError("No grid files found in the given folder.")
+
+
+def views_from_results_folder(results_folder: Path) -> Tuple[View]:
+    """Create a list of views from a results folder.
+
+    Args:
+        results_folder (Path): A directory contianing radiance simulation results from a lbt-recipe.
+
+    Returns:
+        Tuple[SensorGrid]: A tuple of honeybee-radiance Views.
+    """
+
+    if not isinstance(results_folder, Path):
+        results_folder = Path(results_folder)
+
+    # check that one of the following directories exists
+    res_dirs = ["point_in_time_view"]
+    for res_dir in res_dirs:
+        if (results_folder / res_dir).exists():
+            # load views if they exist
+            return [
+                View.from_file(results_folder / res_dir / i)
+                for i in ModelFolder(
+                    project_folder=results_folder / res_dir
+                ).view_files()
+            ]
+
+    raise ValueError("No view files found in the given folder.")
+
+
+def is_sensorgrid_planar(sensorgrid: SensorGrid) -> bool:
+    """Check if a sensorgrid is planar.
+
+    Args:
+        sensorgrid (SensorGrid): A honeybee-radiance SensorGrid.
+
+    Returns:
+        bool: True if the sensorgrid is planar, False otherwise.
+    """
+
+    # create plane
+    plane = Plane.from_three_points(
+        *[Point3D.from_array(i.pos) for i in sensorgrid.sensors[0:3]]
+    )
+    for sensor in sensorgrid.sensors:
+        if not np.isclose(
+            a=plane.distance_to_point(point=Point3D.from_array(sensor.pos)), b=0
+        ):
+            return False
+    return True
+
+
+def plane_from_sensorgrid(sensorgrid: SensorGrid) -> Plane:
+    """Create a plane from a sensorgrid.
+
+    Args:
+        sensorgrid (SensorGrid): A honeybee-radiance SensorGrid.
+
+    Returns:
+        Plane: A ladybug-geometry Plane.
+    """
+
+    plane = Plane.from_three_points(
+        *[Point3D.from_array(i.pos) for i in sensorgrid.sensors[0:3]]
+    )
+
+    if not is_sensorgrid_planar(sensorgrid=sensorgrid):
+        raise ValueError("sensorgrid must be planar to create a plane.")
+
+    return plane
+
+
+def estimate_sensorgrid_spacing(sensorgrid: SensorGrid) -> float:
+    """Estimate the spacing of a sensorgrid.
+
+    Args:
+        sensorgrid (SensorGrid): A honeybee-radiance SensorGrid.
+
+    Returns:
+        float: The estimated spacing (between sensors) of the sensorgrid.
+    """
+    if not isinstance(sensorgrid, SensorGrid):
+        raise ValueError("sensorgrid must be a honeybee-radiance SensorGrid.")
+
+    if not is_sensorgrid_planar(sensorgrid):
+        raise ValueError("sensorgrid must be planar.")
+
+    pts = np.array([i.pos for i in sensorgrid.sensors])
+
+    return np.sort(np.sort(cdist(pts, pts))[:, 1])[-1]
+
+
+def triangulation_from_sensorgrid(
+    sensorgrid: SensorGrid, alpha_adjust: float = 0.1
+) -> Triangulation:
+    """Create matploltib triangulation from a SensorGrid object.
+
+    Args:
+        sensorgrid (SensorGrid): A honeybee-radiance SensorGrid.
+        alpha_adjust (float, optional): A value to adjust the alpha value of the triangulation. Defaults to 0.1.
+
+    Returns:
+        Triangulation: A matplotlib triangulation.
+    """
+
+    warn("This method only currently works for sensor grids in XY plane!")
+
+    alpha = estimate_sensorgrid_spacing(sensorgrid=sensorgrid) + alpha_adjust
+    plane = plane_from_sensorgrid(sensorgrid=sensorgrid)
+
+    # create triangulation
+    x, y, z = np.array([i.pos for i in sensorgrid.sensors]).T
+    return create_triangulation(x, y, alpha=alpha)
+
 
 def spatial_heatmapnew(
     triangulations: List[Triangulation],
--- LadyBugTools_Prototype_Engine/Python/honeybee_extension/daylight_postprocess.py
+++ LadyBugTools_Prototype_Engine/Python/honeybee_extension/daylight_postprocess.py
@@ -9,35 +9,137 @@ from ladybugtools_toolkit.honeybee_extension.results import (
     make_annual,
 )
 import pandas as pd
+from warnings import warn
+from honeybee_radiance_postprocess.en17037 import en17037_to_folder
 
 
 # BR 209
-def side_lit_target_illuminance(
-    illuminance_file: Path, target_illuminance: float = 300
-) -> pd.Series:
+def br209_illuminance_method_c1(
+    illuminance_df: pd.DataFrame,
+    target_illuminance: float,
+    assessment_grid_proportion: float,
+    n_daylight_hours: float = None,
+) -> float:
     """
     Determine the percentage of hours that a target illuminance is met on a sensor grid, and the area over which that target is achieved.
 
-    This is based on BR 209 2022 Site Layout Planning for Daylight and Sunlight: A Guide to Good Practice, Appendix C:Interior daylighting recommendations.
+    Args:
+        illuminance_df (pd.DataFrame):
+            A pandas DataFrame with the illuminance values for each sensor in a single sensor grid. The index of the DataFrame should be timestamps, with each column representing a sensor in the assessment grid.
+        target_illuminance (float):
+            The target illuminance in lux.
+        assessment_grid_proportion (float):
+            The proportion of the sensor grid that must achieve the target illuminance for the result to be considered a pass. Should be a value between 0 and 1.
+        n_daylight_hours (int, optional):
+            The number of daylight hours in the analysis period. Default: None.
 
-    From the above guide, the following is stated:
-    "C5 A target illuminance (ET) should be achieved across at least half of the reference plane in a daylit space for
-    at least half of the daylight hours. Another target illuminance (ETM) should also be achieved across 95% of the
-    reference plane for at least half of the daylight hours; this is the minimum target illuminance to be achieved towards
-    the back of the room."
+    Returns:
+        bool:
+            True if the target illuminance is achieved for at least half of the simulated period, over at the target assessment grid proportion. False otherwise.
 
-    Table C1 - Target illumiannce from daylight over at least half of daylight hours
-    Level of recommendation |                            Target Illuminance                             |
-                            | ET (lx) for half of assessment grid | ETM (lx) for 95% of assessment grid |
-    ------------------------|-------------------------------------|-------------------------------------|
-    Minimum                 | 300                                 | 100                                 |
-    Medium                  | 500                                 | 300                                 |
-    High                    | 750                                 | 500                                 |
-    ------------------------|-------------------------------------|-------------------------------------|
+    Documentation:
+        This is based on BR 209 2022 Site Layout Planning for Daylight and Sunlight: A Guide to Good Practice, Appendix C:Interior daylighting recommendations.
+
+        From the above guide, the following is stated:
+        "C5 A target illuminance (ET) should be achieved across at least half of the reference plane in a daylit space for
+        at least half of the daylight hours. Another target illuminance (ETM) should also be achieved across 95% of the
+        reference plane for at least half of the daylight hours; this is the minimum target illuminance to be achieved towards
+        the back of the room."
+
+        Table C1 - Target illuminance from daylight over at least half of daylight hours
+        Level of recommendation |                            Target Illuminance                             |
+                                | ET (lx) for half of assessment grid | ETM (lx) for 95% of assessment grid |
+        ------------------------|-------------------------------------|-------------------------------------|
+        Minimum                 | 300                                 | 100                                 |
+        Medium                  | 500                                 | 300                                 |
+        High                    | 750                                 | 500                                 |
+        ------------------------|-------------------------------------|-------------------------------------|
     """
 
-    # load the illuminance file
-    ill_values = load_ill([illuminance_file])
-    n_daylit_hours = len(ill_values)
+    if target_illuminance <= 0:
+        raise ValueError("target_illuminance must be greater than zero.")
 
-    # ill_values
+    if assessment_grid_proportion <= 0 or assessment_grid_proportion > 1:
+        raise ValueError("assessment_grid_proportion must be between 0 and 1.")
+
+    if n_daylight_hours is None:
+        n_daylight_hours = len(illuminance_df)
+        warn(
+            f"No value given to n_daylight_hours. The illuminance_df will be assumed to be the same length as daylight-hours per Honeybee-Radiance convention ({n_daylight_hours} hours). If a custom time-period was used for simulation the result given will be incorrect."
+        )
+
+    # checks here to let user know if combinations of target_illuminance and assessment_grid_proportion are not "standard"
+    if (target_illuminance, assessment_grid_proportion) not in [
+        (300, 0.5),
+        (500, 0.5),
+        (750, 0.5),
+        (100, 0.95),
+        (300, 0.95),
+        (500, 0.95),
+    ]:
+        warn(
+            f"target_illuminance of {target_illuminance} and assessment_grid_proportion of {assessment_grid_proportion} are not standard combinations. See BR 209 2022 Appendix C for standard combinations."
+        )
+
+    # determine the number of sensors in the sensor grid
+    n_sensors = len(illuminance_df.columns)
+
+    # create a booean matrix indicating whether the target illuminance has been met
+    achieves_target = illuminance_df > target_illuminance
+
+    # determine whether a proportion of the analysis plane achieves the target for each timestep
+    achieves_spatial = achieves_target.sum(axis=1) > (
+        n_sensors * assessment_grid_proportion
+    )
+
+    # determine whether at least half of the time, the sensors achieve the target
+    achieves_temporal = achieves_spatial.sum() / n_daylight_hours
+
+    if achieves_temporal >= 0.5:
+        return True
+
+    return False
+
+
+def br209_daylightfactor_method_c2(
+    daylight_factor_series: pd.Series,
+    target_daylight_factor: float,
+    assessment_grid_proportion: float,
+) -> float:
+    """
+    Determine whether a target daylight factor is achieved over a given proportion of an assessment grid.
+
+    Args:
+        daylight_factor_series (pd.Series):
+            A pandas Series with the daylight factor values for each sensor in a single sensor grid.
+        target_daylight_factor (float):
+            The target daylight factor, as a value between 0-1. For example 5% would be 0.05.
+        assessment_grid_proportion (float):
+            The proportion of the sensor grid that must achieve the target daylight factor for the result to be considered a pass. Should be a value between 0 and 1.
+
+    Returns:
+        bool:
+            True if the target daylight factor is achieved, over at the target assessment grid proportion. False otherwise.
+
+    Documentation:
+        This is based on BR 209 2022 Site Layout Planning for Daylight and Sunlight: A Guide to Good Practice, Appendix C:Interior daylighting recommendations.
+
+    """
+
+    if not isinstance(daylight_factor_series, pd.Series):
+        raise TypeError("daylight_factor_series must be a pandas Series.")
+
+    if target_daylight_factor <= 0 or target_daylight_factor > 1:
+        raise ValueError("target_daylight_factor must be between 0 and 1.")
+
+    if assessment_grid_proportion <= 0 or assessment_grid_proportion > 1:
+        raise ValueError("assessment_grid_proportion must be between 0 and 1.")
+
+    # determine the number of sensors in the sensor grid
+    n_sensors = len(daylight_factor_series)
+
+    # create a booean matrix indicating whether the target daylight factor has been met
+    achieves_target = daylight_factor_series > target_daylight_factor
+
+    # determine whether at least half of the analysis plane achives the target
+    return achieves_target.sum() > (n_sensors * assessment_grid_proportion)
