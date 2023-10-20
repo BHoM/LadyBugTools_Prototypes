0e0c727920a64bd7006b265f1503d30653a8aed0
Albin Bergh <albin.bergh@burohappold.com>
2023-10-20 17:07:33 +0100

    Small clean up changes

--- LadyBugTools_Prototype_Engine/Python/BoxModel/geometry/BoxModel.py
+++ LadyBugTools_Prototype_Engine/Python/BoxModel/geometry/BoxModel.py
@@ -5,17 +5,8 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from honeybee.model import Model
 from ladybug.wea import Wea
-from .geom import BoxModelSensorGrid
-from ..simulation.daylight_simulation import DaylightSimulation
-from pathlib import Path
-from ..file_utils import make_folder_if_not_exist
-from ..results.daylight_results import DaylightSimResults
-from ..results.daylight_plotting import DaylightPlot, generate_zip
 
 def EpwToWea(epw_file):
-    # epw_file = EPW(epw_filepath)
-
-    # print(epw_file)
     epw_data = epw.EPW(epw_file)
     wea_file = wea.Wea.from_epw_file(epw_file)
     
